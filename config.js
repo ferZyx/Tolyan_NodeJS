a05948430cf7362e10e6c69791080e17cf4ec6c5
ferZyx <sorokin.wlad@yandex.ru>
2023-08-29 18:11:52 +0600

    attemption to fix

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 
 const config = {
-    DEBUG: process.env.DEBUG,
+    DEBUG: process.env.DEBUG === "true",
     DB_URI: process.env.DB_URI,
     TG_TOKEN: process.env.TG_TOKEN,
 }
