1b4d5457c6867446a5bfa893e52d9c4c9d632a3e
ferZyx <sorokin.wlad@yandex.ru>
2023-08-30 12:28:45 +0600

    fixes

--- router.js
+++ router.js
@@ -50,4 +50,11 @@ router.get('/get_user_schedule', async (req, res) => {
 
 })
 
+router.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.send();
+});
+
+
 export default router
