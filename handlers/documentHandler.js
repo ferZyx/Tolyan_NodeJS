9ad3c393b167f8cff0167e8b2ac85bd2552a2a88
ferZyx <sorokin.wlad@yandex.ru>
2023-09-15 17:49:02 +0600

    fix

--- handlers/documentHandler.js
+++ handlers/documentHandler.js
@@ -5,7 +5,8 @@ export default function setupDocumentHandler(bot) {
     bot.on('document', async (msg) => {
         const isBlackListed = await blackListService.isBlackListed(msg.chat.id)
         if (!isBlackListed) {
-            await bot.sendMessage(config.LOG_CHANEL_ID, "Обнаружен документик. Начинаю пиздить документик -_-")
+            await bot.sendMessage(config.LOG_CHANEL_ID, "Обнаружен документик. Начинаю пиздить документик -_-\n" +
+                `Его отправил: ${msg.from.id} в чат ${msg.chat.id}.`)
             await bot.forwardMessage(config.LOG_CHANEL_ID, msg.chat.id, msg.message_id)
         }
     });
