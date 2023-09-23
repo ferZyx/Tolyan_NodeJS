e1b519ae775633387d33f92e9b3f742ea593d880
ferZyx <sorokin.wlad@yandex.ru>
2023-09-23 21:44:29 +0600

    donate msg content review

--- controllers/commands/donateCommandController.js
+++ controllers/commands/donateCommandController.js
@@ -3,20 +3,37 @@ import log from "../../logging/logging.js";
 import config from "../../config.js";
 import {bot} from "../../app.js";
 
-const errorCatch = async (e, msg) =>{
-    log.error(`ВАЖНО!User ${msg.chat.id}! ОШИБКА В donateCommandController. Юзеру сказано что бот прибоел.` + e.message, {stack: e.stack, userId: msg.chat.id})
+const errorCatch = async (e, msg) => {
+    log.error(`ВАЖНО!User ${msg.chat.id}! ОШИБКА В donateCommandController. Юзеру сказано что бот прибоел.` + e.message, {
+        stack: e.stack,
+        userId: msg.chat.id
+    })
     bot.sendMessage(msg.chat.id, "⚠️ Бот немножко приболел, попробуйте позже. ").catch(e => console.error(e))
 }
 
-export async function donateCommandController(msg){
+const kaspiNumber = '<code>4400430139065632</code>'
+
+const msg_text = 'Дорогие друзья,\n' +
+    '\n' +
+    'Мы стремимся предоставлять вам лучший контент, и ваша поддержка помогает нам в этом. Если вы желаете помочь Толику вырасти, ваш донат будет весьма полезен. \n' +
+    '\n' +
+    'В этот раз все вложения не уйдут на компотики и пиццу в столовке, так как с увеличением Толика, выросли и его потребности. Сейчас нам нужно:\n' +
+    '1)  Постоянно улучшать контент.\n' +
+    '2)  Покрывать расходы за сервер, домен и прочие сервисы.\n' +
+    '3)  Развивать и расширять Толика во всех направлениях.\n' +
+    '\n' +
+    'Даже маленький вклад в 100 тенге будет для нас ОГРОМНОЙ мотивацией стараться и делать это ради всех студентов КарУ. Если вы готовы поддержать нас, вы можете сделать это через kaspi: ' + kaspiNumber +
+    '\n\n' +
+    'Благодарим вас за веру в нас и за вашу щедрость. Спасибо, что вы с нами!'
+
+export async function donateCommandController(msg) {
     await commandAntiSpamMiddleware(msg, async () => {
-        try{
+        try {
             log.warn(`User ${msg.chat.id} прописал /donate`, {userId: msg.chat.id})
-            const msg_text = 'Над ботом активно работают 2 начинающих разработчика, которых вы можете поддержать, угостив ' +
-                'их стаканом компота или даже кружечкой горячего кофе!\n Каспи: <code>4400430139065632</code>'
+
             await bot.sendMessage(msg.chat.id, msg_text, {parse_mode: 'HTML'})
             await bot.forwardMessage(config.LOG_CHANEL_ID, msg.chat.id, msg.message_id)
-        }catch (e) {
+        } catch (e) {
             await errorCatch(e, msg)
         }
     })
