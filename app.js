c033ad9dc9003fde7d2f2845e4cd3f2a97ca037f
ferZyx <sorokin.wlad@yandex.ru>
2024-02-01 19:26:05 +0600

    attempt to fix7

--- app.js
+++ app.js
@@ -24,12 +24,7 @@ export const bot = new TelegramBot(config.TG_TOKEN, {
 });
 
 const app = express();
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use("/bot", router);
 app.use(errorMiddleware)
